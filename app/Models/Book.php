b7fd73594caa2d421fd521de2d3a9950b8ac58ce
Eric Uen <ikpassat@gmail.com>
2023-07-28 17:28:55 +0800

    052 Getting Books with Recent Reviews

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -15,13 +16,15 @@ class Book extends Model
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $query, string $title): Builder
+    public function scopeTitle(Builder $query, string $title): Builder | QueryBuilder
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder
     {
-        return $query->withCount('reviews')
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dataRangeFilter($q, $from, $to),
+        ])
             ->orderBy('reviews_count', 'desc');
     }
     public function scopePopularOver(Builder $query, int $num): Builder
@@ -30,10 +33,30 @@ class Book extends Model
             ->orderBy('reviews_count', 'desc')
             ->having('reviews_count', '>=', $num);
     }
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg('reviews', 'rating')
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dataRangeFilter($q, $from, $to),
+        ], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder | QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dataRangeFilter(Builder $query, $from = null, $to = null)
+    {
+
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+
+        }
+    }
 }
